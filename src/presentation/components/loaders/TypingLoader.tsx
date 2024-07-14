dd11bd621dfdce89e5d7434069bb9869d318b5d6
Carlos Manuel González Peña <cmglezpdev@gmail.com>
2024-07-13 22:21:42 -0400

    fix(): set className as optional

--- src/presentation/components/loaders/TypingLoader.tsx
+++ src/presentation/components/loaders/TypingLoader.tsx
@@ -1,7 +1,7 @@
 import './typing-loader.css'
 
 
-export function TypingLoader({ className }: { className: string }) {
+export function TypingLoader({ className }: { className?: string }) {
   return (
     <div className={className + " typing"}>
       <span className="circle scaling"></span>
